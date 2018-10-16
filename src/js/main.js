586fa14805d476b7ab2c4ff284ad7e0fa4db8c51
Regina <regina-saifieva@mail.ru>
2018-10-16 14:42:49 +0300

    Second

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,7 @@ let optional = document.querySelectorAll('.optionalexpenses-item');
 // Получить оставшиеся поля через querySelector (  сумма, процент)
 let pos = document.querySelector('.choose-income'),
 		checkb = document.querySelector('#savings'),
-		sum = document.querySelector('label[for="sum"]'),
+		sum = document.querySelector('choose-sum'),
 		perc = document.querySelector('label[for="percent"]'),
 		year = document.querySelector('.year-value'),
 		month =document.querySelector('.month-value'),
