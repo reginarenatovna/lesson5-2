1c9d02b3d37f4e074534ea7a3ca109a4724f2298
Regina <regina-saifieva@mail.ru>
2018-10-17 22:30:26 +0300

    Second

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,14 @@
 // Получить кнопку "Начать расчет" 
 let start = document.getElementById('start');
 //Получить все блоки в правой части программы через классы 
-let budget = document.getElementsByClassName('budget-value'),
-		daybudget = document.getElementsByClassName('daybudget-value'),
-		level = document.getElementsByClassName('level-value'),
-		expenses = document.getElementsByClassName('expenses-value'),
-		optionalexpenses = document.getElementsByClassName('optionalexpenses-value'),
-		income = document.getElementsByClassName('income-value'),
-		monthsavings = document.getElementsByClassName('monthsavings-value'),
-		yearsavings = document.getElementsByClassName('yearsavings-value');
+let budget = document.getElementsByClassName('budget-value')[0],
+		daybudget = document.getElementsByClassName('daybudget-value')[0],
+		level = document.getElementsByClassName('level-value')[0],
+		expenses = document.getElementsByClassName('expenses-value')[0],
+		optionalexpenses = document.getElementsByClassName('optionalexpenses-value')[0],
+		income = document.getElementsByClassName('income-value')[0],
+		monthsavings = document.getElementsByClassName('monthsavings-value')[0],
+		yearsavings = document.getElementsByClassName('yearsavings-value')[0];
 //Получить поля(input) c обязательными расходами через класс. (class=”expenses-item”)
 let expensesImput = document.getElementsByClassName('expenses-item');
 // Получить кнопки “Утвердить” и “Рассчитать” через Tag, каждую в своей переменной.
